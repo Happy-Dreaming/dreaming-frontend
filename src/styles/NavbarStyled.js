cfa9e17b0d3e62858915d7f393b9ee7235e7bcdf
MyungJiwoo <1206jiwoo@gmail.com>
2024-03-01 02:29:25 +0900

    Design: navbar 수정

--- src/styles/NavbarStyled.js
+++ src/styles/NavbarStyled.js
@@ -7,17 +7,18 @@ export const Container = styled.div`
     width: ${theme.sizes.containerWidth};
     height: 10vh;
 
-    background-color: ${theme.colors.primary};
+    /* background-color: ${theme.colors.primary}; */
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px,
+        rgba(0, 0, 0, 0.1) 0px 2px 4px 0px,
+        rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
+    border-radius: 15px 15px 0 0;
 
     display: flex;
     justify-content: space-evenly;
     align-items: center;
 
-    .menu1,
-    .menu2,
-    .menu3,
-    .menu4,
-    .menu5 {
+    .menu1 {
         width: 15vw;
         height: 15vw;
 
@@ -27,10 +28,11 @@ export const Container = styled.div`
         align-items: center;
 
         font-size: 0.9rem;
-        color: ${theme.colors.primary_character};
+        /* color: ${theme.colors.primary_character}; */
+        color: gray;
     }
 
     .menuIcon {
-        font-size: 1.6rem;
+        font-size: 1.5rem;
     }
 `;
